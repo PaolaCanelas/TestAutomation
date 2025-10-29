ad0535d89804f3dce4dd9d462a2425b7446688e5
Paola <canelaspaola0@gmail.com>
2025-10-29 09:23:33 -0400

    update to ts

---
+++ tests/test/pageobjects/login.page.ts
@@ -0,0 +1,69 @@
+import { ChainablePromiseElement } from 'webdriverio';
+
+export default class LoginPage {
+  private signInTab: ChainablePromiseElement<Promise<WebdriverIO.Element>>;
+  private usernameInput: ChainablePromiseElement<Promise<WebdriverIO.Element>>;
+  private passwordInput: ChainablePromiseElement<Promise<WebdriverIO.Element>>;
+  private signInWithAnyListAccountButton: ChainablePromiseElement<Promise<WebdriverIO.Element>>;
+  private errorBanner: ChainablePromiseElement<Promise<WebdriverIO.Element>>;
+  private okButton: ChainablePromiseElement<Promise<WebdriverIO.Element>>;
+  private homeTitle: ChainablePromiseElement<Promise<WebdriverIO.Element>>;
+  
+  constructor() {
+    this.signInTab = $('//*[@text="Sign In"]');
+    this.usernameInput = $('//*[@text="Email Address"]');
+    this.passwordInput = $('//*[@text="Password"]');
+    this.signInWithAnyListAccountButton = $('//*[@text="Sign In with AnyList Account"]');
+    this.errorBanner = $('//*[@resource-id="com.purplecover.anylist:id/parentPanel"]');
+    this.okButton = $('//*[@resource-id="com.purplecover.anylist:id/parentPanel"]');
+    this.homeTitle = $('//*[@text="Lists"]');
+  }
+
+  async openLoginScreen() {
+    const getStartedButton = await $('//*[@text="Get Started"]');
+    if (await getStartedButton.isDisplayed()) {
+      await getStartedButton.click();
+    }
+
+    await this.signInTab.waitForDisplayed({ timeout: 10000 });
+    await this.signInTab.click();
+  }
+
+  async enterUsername(username: string) {
+    await this.usernameInput.waitForDisplayed({ timeout: 5000 });
+    await this.usernameInput.clearValue();
+    await this.usernameInput.setValue(username);
+  }
+
+  async enterPassword(password: string) {
+    await this.passwordInput.waitForDisplayed({ timeout: 5000 });
+    await this.passwordInput.clearValue();
+    await this.passwordInput.setValue(password);
+  }
+
+  async tapLogin() {
+    await this.signInWithAnyListAccountButton.waitForDisplayed({ timeout: 5000 });
+    await this.signInWithAnyListAccountButton.click();
+  }
+
+  async login(username: string, password: string) {
+    await this.enterUsername(username);
+    await this.enterPassword(password);
+    await this.tapLogin();
+  }
+
+  async getErrorText(): Promise<string> {
+    await this.errorBanner.waitForDisplayed({ timeout: 10000 });
+    return await this.errorBanner.getText();
+  }
+
+  async confirmError() {
+    await this.okButton.waitForDisplayed({ timeout: 5000 });
+    await this.okButton.click();
+  }
+
+  async isHomeDisplayed(): Promise<boolean> {
+    await this.homeTitle.waitForDisplayed({ timeout: 10000 });
+    return await this.homeTitle.isDisplayed();
+  }
+}
